31b1925da04da2a23d5a7f0482b886fd82df8f3f
Lunatoid <lunatoid@pm.me>
2019-12-12 14:57:05 +0100

    Added collider to water and water bopping

---
+++ DevilPocket/Assets/Scripts/WaterBop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBop : MonoBehaviour {
+
+    [SerializeField]
+    float forceAmount = 1000000.0f;
+
+    void OnTriggerStay(Collider other) {
+        Debug.Log("Collision with " + other.name);
+
+        if (other.tag == "Player") {
+            other.GetComponent<CharacterController>().Move(Vector3.up * forceAmount * Time.deltaTime);
+        }
+    }
+}
