8dec22bab31cbe443b9ed95c4bd38afee51284a0
Lunatoid <lunatoid@pm.me>
2020-01-18 14:53:04 +0100

    Actually added prefabs/scripts

---
+++ DevilPocket/Assets/Scripts/Battle/RandomMonsterPicker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMonsterPicker : MonoBehaviour {
+
+    [System.Serializable]
+    struct MonsterEntry {
+        [Header("% chance"), Range(0.0f, 1.0f)]
+        public float percentChance;
+        public GameObject monsterPrefab;
+    }
+
+    [SerializeField]
+    List<MonsterEntry> randomMonsters = new List<MonsterEntry>();
+    System.Random random = new System.Random();
+
+    private void Awake() {
+        DontDestroyOnLoad(this);
+    }
+
+    void ShuffleList() {
+        // Fisher-Yates shuffle.
+        int n = randomMonsters.Count;
+        while (n > 1) {
+            --n;
+            int k = random.Next(n + 1);
+            MonsterEntry tmp = randomMonsters[k];
+            randomMonsters[k] = randomMonsters[n];
+            randomMonsters[n] = tmp;
+        }
+    }
+
+    /// <summary>
+    /// Returns a random monster based on their appearance chance.
+    /// </summary>
+    /// <returns>A random monster based on their appearance chance.</returns>
+    public GameObject GetRandomMonsterPrefab() {
+        // Shuffle the list so we don't have any bias related to the position within the array
+        ShuffleList();
+
+        foreach (MonsterEntry entry in randomMonsters) {
+            if (Random.Range(0.0f, 1.0f) <= entry.percentChance) {
+                // The check passed, this is the one.
+                return entry.monsterPrefab;
+            }
+        }
+
+        // If no monster returned, just return the first item in the list, which should still be random thanks to the shuffle
+        return randomMonsters[0].monsterPrefab;
+    }
+}
