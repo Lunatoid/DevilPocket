d947882d08ff5b154592fc8ef39db38d4bc9c266
joris kleijn <joriskleijn2000@gmail.com>
2020-01-24 12:17:43 +0100

    reactor poeg NPC doet kut

--- DevilPocket/Assets/Scripts/reactor/OpenReactorDoorsInside.cs
+++ DevilPocket/Assets/Scripts/reactor/OpenReactorDoorsInside.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenReactorDoorsInside : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class OpenReactorDoorsInside : MonoBehaviour {
+
+
+    public Animator openingDoors;
+
+
+    bool iceBeaten = false;
+    bool metalBeaten = false;
+    bool poisonBeaten = false;
+    bool godBeaten = false;
+
+    private void Start() {
+        //openingDoors = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    // 5 functions that wil order the opening of the doors
+    public void OpenFirstDoor(List<string> args) {
+        Debug.Log("Open deur");
+        // openingDoors.SetTrigger("First");
+        openingDoors.SetTrigger("Ice");
+    }
+
+    public void OpenSecondDoor(List<string> args) {
+        if (iceBeaten == true) {
+            openingDoors.SetTrigger("Ice");
+        }
+    }
+
+    public void OpenThirdDoor(List<string> args) {
+        if (metalBeaten == true) {
+            openingDoors.SetTrigger("Metal");
+        }
+    }
+
+    public void OpenForthDoor(List<string> args) {
+        if (poisonBeaten == true) {
+            openingDoors.SetTrigger("Poison");
+        }
+    }
+
+    public void OpenFifthDoor(List<string> args) {
+        if (godBeaten == true) {
+            openingDoors.SetTrigger("God");
+        }
     }
 }
