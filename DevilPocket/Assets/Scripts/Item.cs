45e8de6bb2febd14767da145876165af7d7c9e4f
joris kleijn <joriskleijn2000@gmail.com>
2020-01-27 15:09:50 +0100

    shop ui

---
+++ DevilPocket/Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+public class Item : MonoBehaviour {
+
+    public enum ItemType {
+        None,
+        Aas,
+        Paracetamol,
+        Ibuprofen,
+        Morphine,
+        BattlePass
+    }
+
+    //item pricees 
+    public static int GetCost(ItemType itemType) {
+        switch (itemType) {
+            default:
+            case ItemType.None:         return 0;
+            case ItemType.Aas:          return 20;
+            case ItemType.Paracetamol:  return 20;
+            case ItemType.Ibuprofen:    return 50;
+            case ItemType.Morphine:     return 100;
+            case ItemType.BattlePass:   return 1500;
+        }
+    }
+}
---
+++ DevilPocket/Assets/Scripts/Shop_UI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Shop_UI : MonoBehaviour
+{
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    public Sprite aas;
+    public Sprite paracetamol;
+    public Sprite ibuprofen;
+    public Sprite morphine;
+    public Sprite battlePass;
+
+    private void Awake() {
+        container = transform.Find("container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start() {
+        CreateItemButton(aas, "Aas", Item.GetCost(Item.ItemType.Aas),0);
+        CreateItemButton(paracetamol, "Paracetamol", Item.GetCost(Item.ItemType.Paracetamol), 1);
+        CreateItemButton(ibuprofen, "Ibuprofen", Item.GetCost(Item.ItemType.Ibuprofen), 2);
+        CreateItemButton(morphine, "Morphine", Item.GetCost(Item.ItemType.Morphine), 3);
+        CreateItemButton(battlePass, "Battle pass", Item.GetCost(Item.ItemType.BattlePass),4);
+    }
+
+    private void CreateItemButton(Sprite itemSprite, string itemName, int itemCost, int positionIndex) {
+        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+
+        float shopItemHeight = 30.0f;
+        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+
+        shopItemTransform.Find("ItemText").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemTransform.Find("valueText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+
+        shopItemTransform.Find("").GetComponent<Image>().sprite = itemSprite;
+    }
+
+
+}
