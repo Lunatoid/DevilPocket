fe9d55abf1fdbe4c6bf129b993112b19818c0d40
joris kleijn <joriskleijn2000@gmail.com>
2020-01-28 21:49:08 +0100

    spul

--- DevilPocket/Assets/Scripts/Battle/HealItem.cs
+++ DevilPocket/Assets/Scripts/Battle/HealItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HealItem : MonoBehaviour {
     public enum HealType {
@@ -9,26 +10,46 @@ public class HealItem : MonoBehaviour {
         morphine
     }
 
+    PlayerInventory playerInventory;
+
+    Monster monster;
+
+    public void ParacetamolButtonClik() {
+        ItemHeal(HealType.paracetamol, playerInventory, monster);
+    }
+
+    public void IbuprofenButtonClik() {
+        ItemHeal(HealType.ibuprofen, playerInventory, monster);
+    }
+
+    public void MorphineButtonClik() {
+        ItemHeal(HealType.morphine, playerInventory, monster);
+    }
+
+
     public void ItemHeal(HealType healType, PlayerInventory playerInventory, Monster monster) {
         switch (healType) {
             case HealType.paracetamol:
-                if (playerInventory.paracetamolAmount > 0) {
+                if (playerInventory.paracetamolAmount > 0) { // gives here an error
                     monster.currentHP = monster.currentHP + (monster.maxHP / 3);
                     playerInventory.paracetamolAmount--;
+                    Debug.Log("Paracetamol");
                 }
                 break;
 
             case HealType.ibuprofen:
-                if (playerInventory.ibuprofenAmount > 0) {
+                if (playerInventory.ibuprofenAmount > 0) { // gives here an error
                     monster.currentHP = monster.currentHP + (monster.maxHP / 2);
                     playerInventory.ibuprofenAmount--;
+                    Debug.Log("ibuprofen");
                 }
                 break;
 
             case HealType.morphine:
-                if (playerInventory.morphineAmount > 0) {
+                if (playerInventory.morphineAmount > 0) { // gives here an error
                     monster.currentHP = monster.maxHP;
                     playerInventory.morphineAmount--;
+                    Debug.Log("morphine");
                 }
                 break;
         }
