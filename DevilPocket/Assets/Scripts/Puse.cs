e495de9c6a56b4ad9a60e1e14f5362a8ef88c980
joris kleijn <joriskleijn2000@gmail.com>
2020-02-01 00:07:02 +0100

    sleep sleep

--- DevilPocket/Assets/Scripts/Puse.cs
+++ DevilPocket/Assets/Scripts/Puse.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
 public class Puse : MonoBehaviour {
+
     [SerializeField] private GameObject pausePanel;
 
+    LoadTransition lt;
 
     private void Awake() {
         pausePanel.SetActive(false);
@@ -23,17 +25,26 @@ public class Puse : MonoBehaviour {
         }
     }
 
-
     private void PauseGame() {
         Time.timeScale = 0;
         pausePanel.SetActive(true);
         Debug.Log("pouse");
     }
 
-
-    private void ContinueGame() {
+    public void ContinueGame() {
         Time.timeScale = 1;
         pausePanel.SetActive(false);
         Debug.Log("Play");
     }
+
+    public void LoadMenuScene() {
+        StartCoroutine(LoadMenuSceneCO());
+    }
+
+    IEnumerator LoadMenuSceneCO() {
+        lt.FadeToBlack();
+        yield return new WaitForSeconds(1.1f);
+        AsyncOperation op = SceneManager.LoadSceneAsync("MenuScene");
+    }
+
 }
