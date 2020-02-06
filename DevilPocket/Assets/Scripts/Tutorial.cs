ac30f1a7e088c6675cc058889b2dab8981a8d81b
joris kleijn <joriskleijn2000@gmail.com>
2020-02-06 23:05:42 +0100

    post procesing, 2 qwests, epic ness

---
+++ DevilPocket/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject playermonster1;
+
+    private void Start() {
+       // playermonster1.SetActive(false);  
+    }
+
+    public void StepOneDone() {
+        playermonster1.SetActive(true);
+    }
+
+    
+
+}
